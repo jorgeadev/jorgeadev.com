4876bc534246eba2dd1a14a2148295bfe9b644b3
Jorge Gomez <jorgealbertogomezgomez77@gmail.com>
2025-11-18 22:10:45 -0500

    Pass session prop to SignInButtonHome component
    Updated the Navigation component to provide an empty string as the session prop to the SignInButtonHome component, ensuring it receives the expected prop.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -29,7 +29,7 @@ export const Navigation = () => {
 								Support Us
 							</Link>
 
-							<SignInButtonHome />
+							<SignInButtonHome session={""} />
 						</div>
 					</div>
 				</div>
